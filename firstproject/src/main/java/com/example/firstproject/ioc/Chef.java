4ab89e9ea5ba6d01540a8ab35e2f510d0c495edc
오정수 <oh050211@gmail.com>
2023-01-12 21:35:42 +0900

    firstproject

---
+++ firstproject/src/main/java/com/example/firstproject/ioc/Chef.java
@@ -0,0 +1,10 @@
+package com.example.firstproject.ioc;
+
+public class Chef {
+    public String cook(String menu) {
+        // 재료 준비
+        Pork pork = new Pork("한돈 등심");
+        // 요리 반환
+        return pork.getName() + "으로 만든 " + menu;
+    }
+}
---
+++ firstproject/src/main/java/com/example/firstproject/ioc/Pork.java
@@ -0,0 +1,13 @@
+package com.example.firstproject.ioc;
+
+public class Pork {
+    private String name;
+
+    public Pork(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
