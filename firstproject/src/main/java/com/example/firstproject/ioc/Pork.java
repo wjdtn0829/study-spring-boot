43ed4a3761a03c29ceaabf430263a6a5d1a20008
오정수 <oh050211@gmail.com>
2023-01-15 23:41:50 +0900

    firstproject

--- firstproject/src/main/java/com/example/firstproject/ioc/Pork.java
+++ firstproject/src/main/java/com/example/firstproject/ioc/Pork.java
@@ -1,13 +1,12 @@
 package com.example.firstproject.ioc;
 
-public class Pork {
-    private String name;
-
+public class Pork extends Ingredient {
     public Pork(String name) {
-        this.name = name;
+        super(name);
     }
-
-    public String getName() {
-        return name;
-    }
+//    private String name;
+//
+//    public Pork(String name) { this.name = name; }
+//
+//    public String getName() { return name; }
 }
