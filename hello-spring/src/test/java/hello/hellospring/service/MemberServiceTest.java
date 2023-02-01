f2eae5289eb91750bc49017da8d7b4d10f10d361
wjdtn0829 <oh050211@gmail.com>
2023-02-01 23:51:10 +0900

    hello-spring

---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+
+        // then
+
+    }
+
+    @Test
+    void findMembers() {Member member1 = new Member();
+        member1.setName("spring");
+    }
+
+    @Test
+    void findOne() {
+    }
+}
